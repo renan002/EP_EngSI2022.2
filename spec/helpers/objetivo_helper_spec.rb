46cafd8479efdc55b0a0f643d9750df321834355
Ivo-Andrade <leg.dev@protonmail.com>
2022-11-22 16:20:03 -0300

    [I4] Atualização do nome para padrão de plural

--- spec/helpers/objetivo_helper_spec.rb
+++ spec/helpers/objetivo_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe ObjetivoHelper, type: :helper do
+RSpec.describe ObjetivosHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end

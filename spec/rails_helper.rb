130f797b4e51fd77b096adc5f32f5c442750c146
Renan Mastropaolo <renan_mastropaolo@dell.com>
2022-12-03 18:35:09 -0300

    [I5] Corrigindo falha no cucumber

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,18 +1,5 @@
 require 'simplecov'
-SimpleCov.start 'rails' do
-  if ENV['CI']
-    require 'simplecov-lcov'
-
-    SimpleCov::Formatter::LcovFormatter.config do |c|
-      c.report_with_single_file = true
-      c.single_report_path = 'coverage/lcov.info'
-    end
-
-    formatter SimpleCov::Formatter::LcovFormatter
-  end
-
-  add_filter %w[version.rb initializer.rb]
-end
+require 'simplecov-lcov'
 require 'support/factory_bot'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'

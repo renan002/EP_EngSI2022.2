bd16a153d50518be3f13c385f8245694c65abe99
Renan Mastropaolo <re.gon.mas@usp.br>
2022-12-06 18:55:09 -0300

    [I5] Arrumando o cucumber para parar de tentar enviar o report para o Coveralls

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,3 @@
-require 'simplecov'
-require 'simplecov-lcov'
-require 'support/factory_bot'
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,26 +1,12 @@
-require 'simplecov'
-SimpleCov.start 'rails' do
-  if ENV['CI']
-    require 'simplecov-lcov'
-
-    SimpleCov::Formatter::LcovFormatter.config do |c|
-      c.report_with_single_file = true
-      c.single_report_path = 'coverage/lcov.info'
-    end
-
-    formatter SimpleCov::Formatter::LcovFormatter
-  end
-
-  add_filter %w[version.rb initializer.rb]
-end
-
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require 'simplecov'
+require 'simplecov-lcov'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors, with: :threads)
+  parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all

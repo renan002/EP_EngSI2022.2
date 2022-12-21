dfe8ab2b8c79f408d2b701fd9a4e72c1f299ac3c
Renan Gonçalves Mastropaolo <re.gon.mas@usp.br>
2022-12-21 17:14:36 -0300

    [I6] Aumento de testes

---
+++ features/exibir_usuarios.feature
@@ -0,0 +1,11 @@
+#language: pt
+
+Funcionalidade: Exibir conta de usuário
+Como um usuário cadastrado
+Eu quero abrir a tela de usuarios
+Para exibir todos os usuarios
+
+Cenário: Cadastro com sucesso
+Dado que fui autenticado com sucesso
+Quando eu abro página de usuarios
+Então todos os usuarios serao exibidos
--- features/step_definitions/exibir_pagina_principal.rb
+++ features/step_definitions/exibir_pagina_principal.rb
@@ -1,9 +1,16 @@
 Dado('que fui autenticado com sucesso') do
   visit '/usuarios/new'
-  fill_in "Email", :with => "teste@gmail.com"
-  fill_in "Password", :with => "123456"
+    fill_in "Nome", :with => "Test User"
+    fill_in "Email", :with => "teste@gmail.com"
+    fill_in "Password", :with => "123456"
+    fill_in "Password confirmation", :with => "123456"
+    click_on "Cadastrar"
 end
   
-Então('exibe página principal') do
-  visit '/usuarios/show'
+Quando('eu abro página principal') do
+  visit '/usuarios'
+end
+
+Então('todos os usuarios serao exibidos') do
+  expect(page).to have_content('Usuários')
 end
